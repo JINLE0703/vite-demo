b0fbda91ddd7569250d235e33714d81abe453046
Jinle <jinle0703@foxmail.com>
2021-09-12 16:06:20 +0800

    feat: 杂碎知识

--- vite-vue3/src/App.jsx
+++ vite-vue3/src/App.jsx
@@ -1,11 +1,28 @@
 import { defineComponent } from 'vue';
 
-import 'styles/index.css'
+import 'styles/index.css';
+import 'styles/test.scss';
+
+import Test from './test?raw';
+console.log(Test); // 文件内容
+
+import pak from '../package.json';
+console.log(pak);
+
+import classes from 'styles/test.module.css';
+
+import logo from './assets/logo.png';
 
 export default defineComponent({
-    setup() {
-        return () => {
-            return <div class="text-blue">Hello vue3 jsx</div>
-        }
-    }
-})
+  setup() {
+    return () => {
+      return (
+        <div class={`text-blue root ${classes.moduleClass}`}>
+          Hello vue3 jsx
+          {/* <div>my name is {jinle.name}</div> */}
+          <img src={logo} />
+        </div>
+      );
+    };
+  },
+});
---
+++ vite-vue3/src/test.ts
@@ -0,0 +1,7 @@
+import { Person } from './type'
+
+export const jinle: Person = {
+    name: 'Jinle'
+}
+
+// export { Person };
