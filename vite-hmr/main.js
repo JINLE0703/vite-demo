2a90e1ed3887c717082f97145cf93e99b1d1e236
jinle <jinle0703@foxmail.com>
2021-09-15 20:43:07 +0800

    feat: glob

--- vite-hmr/main.js
+++ vite-hmr/main.js
@@ -1,6 +1,16 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// 切记需要export
+export function render() {
+  document.querySelector('#app').innerHTML = `
+    <h1>Hello Vite!</h1>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation1</a>
+  `
+}
+
+// build的代码没有hot，开发环境才需要开启热更新
+if(import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    newModule.render();
+  });
+}
--- vite-vue3/src/main.js
+++ vite-vue3/src/main.js
@@ -1,6 +1,14 @@
-import {
-    createApp
-} from 'vue'
-import App from './App'
+import { createApp } from 'vue';
+import App from './App';
 
-createApp(App).mount('#app')
+const modules = import.meta.globEager('./components/glob/*');
+
+console.log('glob', modules);
+
+// for(const path in modules) {
+//     modules[path]().then((m) => {
+//         console.log(path, m.default);
+//     })
+// }
+
+createApp(App).mount('#app');
